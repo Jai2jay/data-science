d521a3dd7c8d031e658a0005fc9b7bbddf73a030
jai <adithjai2004@gmail.com>
2025-08-23 12:29:01 +0530

    kuttan

--- panda.py
+++ panda.py
@@ -1,5 +1,11 @@
 import pandas as p
-data = {'name':['alice','bob'],'age':[3,5]} 
+data = {'name':['alice','bob','cat','dog','kuttan','ger'],'score':[3,5,6,80,4,7]} 
 d=p.DataFrame(data)
 print(d)
 
+print(d.head(2))
+print(d.tail())
+
+k=p.DataFrame(data)
+d.info()
+print(d.describe())
